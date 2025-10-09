ee9a4e8b1d55dd9129e179ea173478bd40a393ca
Dah <abdelrahmenbenfarhat@gmail.com>
2025-10-09 21:25:13 +0100

    finished los pollos hermanos!

---
+++ html/TributeProject/script.js
@@ -0,0 +1,19 @@
+/*
+let cursorPosition = { x: 0, y: 0 };
+const sectionHeader = document.querySelector('.section-header');
+*/
+/*document.addEventListener('mousemove', (event) => {
+  const x = event.clientX; // Horizontal position
+  const y = event.clientY; // Vertical position
+  cursorPosition = { x, y };
+  console.log(`Cursor Position: X: ${x}, Y: ${y}`);
+});*/
+
+/*
+fetch('./components/timeline.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('timeline').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading HTML:', error));
+    */
